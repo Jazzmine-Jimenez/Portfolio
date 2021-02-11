4916b1daebad798ae0476576f73e0afa463970b8
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2021-02-10 20:17:08 -0800

    about me completed

--- distribution/vendor/bootstrap/js/bootstrap.bundle.js
+++ distribution/vendor/bootstrap/js/bootstrap.bundle.js
@@ -9,7 +9,7 @@
   (global = global || self, factory(global.bootstrap = {}, global.jQuery));
 }(this, function (exports, $) { 'use strict';
 
-  $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
+  const $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
 
   function _defineProperties(target, props) {
     for (var i = 0; i < props.length; i++) {
